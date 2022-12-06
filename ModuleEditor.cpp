f08dd5b303a2e4b659f75586b0dda2ed5d3fd142
santiagocarbo89 <carbo@correo.ugr.es>
2022-12-06 13:08:24 +0100

    Module Editor

--- ModuleEditor.cpp
+++ ModuleEditor.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 
-ModuleEditor::ModuleEditor()
+ModuleEditor::ModuleEditor() : showProperties(true), showConfiguration(true), showAssimpConsole(true)
 {}
 
 ModuleEditor::~ModuleEditor()
@@ -14,6 +14,11 @@ bool ModuleEditor::Init()
 	ImGui::CreateContext();
 	io = ImGui::GetIO();
 	io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;
+
+	generalDisplayProportions = ImVec2(App->GetWindow()->GetWidth() / 9.0f, App->GetWindow()->GetHeight() / 4.0f);
+	propertiesDisplayProportions = ImVec2(App->GetWindow()->GetWidth() / 9.0f, App->GetWindow()->GetHeight() / 4.0f);
+	configurationDisplayProportions = ImVec2(App->GetWindow()->GetWidth() / 9.0f, App->GetWindow()->GetHeight() / 4.0f);
+	assimpConsoleDisplayProportions = ImVec2(App->GetWindow()->GetWidth() / 9.0f, App->GetWindow()->GetHeight() / 4.0f);
 	
 	return true;
 }
@@ -37,31 +42,62 @@ update_status ModuleEditor::PreUpdate()
 
 update_status ModuleEditor::Update()
 {
+	io.DisplaySize = generalDisplayProportions;
+
 	ImGui::Begin("General Menu");
-	ImGui::Text("Engine Name: Sakuraba Engine");
+
+	ImGui::Text("GitHub Page: %s", "https://github.com/sncarbo/FirstAssigment");
+	ImGui::Text("Engine Name: %s", TITLE);
 	ImGui::Text("Author: Santiago Carbo Garcia");
+
 	ImGui::End();
 
-	ImGui::Begin("Configuration menu");
-
-	ImGui::PlotHistogram("##framerate", nullptr, 0, 0, 0, "Frames Per Second", 0.0f, 100.0f, ImVec2(310, 100));
-
-	if (ImGui::BeginMenuBar())
+	if (showProperties)
 	{
-		if (ImGui::BeginMenu("Renderer variables"))
-		{
-			if (ImGui::MenuItem("Open..", "Ctrl+O"))
-			{
+		io.DisplaySize = propertiesDisplayProportions;
 
-			}
-			
-			ImGui::EndMenu();
-		}
+		ImGui::Begin("Properties Menu");
 
-		ImGui::EndMenuBar();
+		ImGui::End();
 	}
 
-	ImGui::End();
+	if (showConfiguration)
+	{
+		io.DisplaySize = configurationDisplayProportions;
+
+		ImGui::Begin("Configuration menu");
+
+		fpsLog.push_back(1.0f/io.DeltaTime);
+
+		sprintf_s(fpsTitleBuffer, sizeof(fpsTitleBuffer), "FPS %.1f", fpsLog[fpsLog.size() - 1]);
+		ImGui::PlotHistogram("##framerate", &fpsLog[0], fpsLog.size(), 0, "FPS", 0.0f, 100.0f, ImVec2(310, 100));
+
+		if (ImGui::BeginMenuBar())
+		{
+			if (ImGui::BeginMenu("Renderer variables"))
+			{
+				if (ImGui::MenuItem("Open..", "Ctrl+O"))
+				{
+
+				}
+
+				ImGui::EndMenu();
+			}
+
+			ImGui::EndMenuBar();
+		}
+
+		ImGui::End();
+	}
+
+	if (showAssimpConsole)
+	{
+		io.DisplaySize = assimpConsoleDisplayProportions;
+
+		ImGui::Begin("Assimp Console");
+
+		ImGui::End();
+	}
 
 	ImGui::Render();
 	ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
--- ModuleEditor.h
+++ ModuleEditor.h
@@ -3,14 +3,18 @@
 #include "Globals.h"
 #include "Module.h"
 #include "Application.h"
-#include "ModuleRender.h"
-#include "ModuleWindow.h"
+
 #include "ShadersProgram.h"
+
 #include "glew.h"
 #include "imgui.h"
 #include "imgui_impl_opengl3.h"
 #include "imgui_impl_sdl.h"
+
+#include <vector>
 #include <string>
+
+using namespace std;
 
 class ModuleEditor : public Module
 {
@@ -27,6 +31,17 @@ public:
 	bool CleanUp();
 
 private:
+	ImVec2 generalDisplayProportions;
+	ImVec2 propertiesDisplayProportions;
+	ImVec2 configurationDisplayProportions;
+	ImVec2 assimpConsoleDisplayProportions;
+
+	char fpsTitleBuffer[25];
+	vector<float> fpsLog;
+
+	bool showProperties;
+	bool showConfiguration;
+	bool showAssimpConsole;
 
 	ImGuiIO io;
 };
